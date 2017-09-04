9323a78dca976fec7fe8429933ee4b017f8da13b
Eric Nograles <grales@gmail.com>
2017-09-04 17:03:14 -0400

    adds unit test for remote database URL parsing

--- db_test.go
+++ db_test.go
@@ -2,10 +2,19 @@ package main
 
 import "testing"
 
-func TestParseDatabaseURL(t *testing.T) {
+func TestParseLocalDatabaseURL(t *testing.T) {
 	testURL := "postgres://postgres@127.0.0.1:5432/test_db"
 	parsedURL := parseDatabaseURL(testURL)
 	expectedParsedURL := "host=127.0.0.1 port=5432 user=postgres dbname=test_db sslmode=disable"
+	if parsedURL != expectedParsedURL {
+		t.Errorf("Database URL was incorrect. Expected %v but got %v", expectedParsedURL, parsedURL)
+	}
+}
+
+func TestParseRemoteDatabaseURL(t *testing.T) {
+	testURL := "postgres://postgres:password@some-ec2-server.aws.com:5432/test_db"
+	parsedURL := parseDatabaseURL(testURL)
+	expectedParsedURL := "host=some-ec2-server.aws.com port=5432 user=postgres password=password dbname=test_db sslmode=require"
 	if parsedURL != expectedParsedURL {
 		t.Errorf("Database URL was incorrect. Expected %v but got %v", expectedParsedURL, parsedURL)
 	}
